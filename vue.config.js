90155844d0608b0c545d84ce4e8d76aadbd6e04e
czkiam <czkiam@gmail.com>
2020-02-14 10:17:02 +0800

    clean up package.json add electron-builder config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,36 @@
 // vue.config.js
 module.exports = {
-    configureWebpack: config => {
-      if (process.env.NODE_ENV === 'production') {
-        // mutate config for production...
-      } else {
-        // mutate for development...
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        appId: 'electron.vue.f7.test',
+        mac: {
+          category: 'your.app.category.type'
+        },
+        linux: {
+          target: {
+            target: 'deb',
+            arch: ['armv7l']
+          },
+          category: 'Utility'
+        },
+        win: {
+          target: [
+            {
+              target: 'nsis',
+              arch: ['x64', 'ia32']
+            }
+          ]
+        }
       }
     }
   }
+};
