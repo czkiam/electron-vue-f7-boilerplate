0694beaeef9a4980b12c31193a96e4a406788e0d
czkiam <czkiam@gmail.com>
2020-02-15 14:50:23 +0800

    add sqlite3 support

--- src/app.vue
+++ src/app.vue
@@ -1,113 +1,129 @@
 <template>
-<f7-app :params="f7params" >
+  <f7-app :params="f7params">
+    <!-- Left panel with cover effect-->
+    <f7-panel left cover theme-dark>
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Left Panel"></f7-navbar>
+          <f7-block>Left panel content goes here</f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
 
-  <!-- Left panel with cover effect-->
-  <f7-panel left cover theme-dark>
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Left Panel"></f7-navbar>
-        <f7-block>Left panel content goes here</f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-panel>
+    <!-- Right panel with reveal effect-->
+    <f7-panel right reveal theme-dark>
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Right Panel"></f7-navbar>
+          <f7-block>Right panel content goes here</f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
 
+    <!-- Your main view, should have "view-main" class -->
+    <f7-view main class="safe-areas" url="/"></f7-view>
 
-  <!-- Right panel with reveal effect-->
-  <f7-panel right reveal theme-dark>
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Right Panel"></f7-navbar>
-        <f7-block>Right panel content goes here</f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-panel>
+    <!-- Popup -->
+    <f7-popup id="my-popup">
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Popup">
+            <f7-nav-right>
+              <f7-link popup-close>Close</f7-link>
+            </f7-nav-right>
+          </f7-navbar>
+          <f7-block>
+            <p>Popup content goes here.</p>
+          </f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-popup>
 
-
-  <!-- Your main view, should have "view-main" class -->
-  <f7-view main class="safe-areas" url="/"></f7-view>
-
-
-  <!-- Popup -->
-  <f7-popup id="my-popup">
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Popup">
-          <f7-nav-right>
-            <f7-link popup-close>Close</f7-link>
-          </f7-nav-right>
-        </f7-navbar>
-        <f7-block>
-          <p>Popup content goes here.</p>
-        </f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-popup>
-
-  <f7-login-screen id="my-login-screen">
-    <f7-view>
-      <f7-page login-screen>
-        <f7-login-screen-title>Login</f7-login-screen-title>
-        <f7-list form>
-          <f7-list-input
-            type="text"
-            name="username"
-            placeholder="Your username"
-            :value="username"
-            @input="username = $event.target.value"
-          ></f7-list-input>
-          <f7-list-input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            :value="password"
-            @input="password = $event.target.value"
-          ></f7-list-input>
-        </f7-list>
-        <f7-list>
-          <f7-list-button title="Sign In" @click="alertLoginData"></f7-list-button>
-          <f7-block-footer>
-            Some text about login information.<br>Click "Sign In" to close Login Screen
-          </f7-block-footer>
-        </f7-list>
-      </f7-page>
-    </f7-view>
-  </f7-login-screen>
-</f7-app>
+    <f7-login-screen id="my-login-screen">
+      <f7-view>
+        <f7-page login-screen>
+          <f7-login-screen-title>Login</f7-login-screen-title>
+          <f7-list form>
+            <f7-list-input
+              type="text"
+              name="username"
+              placeholder="Your username"
+              :value="username"
+              @input="username = $event.target.value"
+            ></f7-list-input>
+            <f7-list-input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              :value="password"
+              @input="password = $event.target.value"
+            ></f7-list-input>
+          </f7-list>
+          <f7-list>
+            <f7-list-button title="Sign In" @click="alertLoginData"></f7-list-button>
+            <f7-block-footer>
+              Some text about login information.
+              <br />Click "Sign In" to close Login Screen
+            </f7-block-footer>
+          </f7-list>
+        </f7-page>
+      </f7-view>
+    </f7-login-screen>
+  </f7-app>
 </template>
 <script>
+import routes from './routes.js';
 
-  import routes from './routes.js';
+export default {
+  data() {
+    return {
+      // Framework7 Parameters
+      f7params: {
+        name: 'My App', // App name
+        theme: 'auto', // Automatic theme detection
 
-  export default {
-    data() {
-      return {
-        // Framework7 Parameters
-        f7params: {
-          name: 'My App', // App name
-          theme: 'auto', // Automatic theme detection
+        // App routes
+        routes: routes
+      },
 
+      // Login screen data
+      username: '',
+      password: ''
+    };
+  },
+  mounted() {
+    this.$f7ready(f7 => {
+      // Call F7 APIs here
+      var sqlite3 = require('sqlite3').verbose();
+      var db = new sqlite3.Database('./database.sqlite3');
 
-          // App routes
-          routes: routes,
-        },
+      db.serialize(function() {
+        db.run('CREATE TABLE lorem (info TEXT)');
 
-        // Login screen data
-        username: '',
-        password: '',
-      }
-    },
-    methods: {
-      alertLoginData() {
-        this.$f7.dialog.alert('Username: ' + this.username + '<br>Password: ' + this.password, () => {
-          this.$f7.loginScreen.close();
+        var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+        for (var i = 0; i < 10; i++) {
+          stmt.run('Ipsum ' + i);
+        }
+
+        stmt.finalize();
+
+        var rows = document.getElementById('database');
+        db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+          var item = document.createElement('li');
+          item.textContent = '' + row.id + ': ' + row.info;
+          rows.appendChild(item);
         });
-      }
-    },
-    mounted() {
-      this.$f7ready((f7) => {
+      });
 
-        // Call F7 APIs here
+      db.close();
+    });
+  },
+  methods: {
+    alertLoginData() {
+      this.$f7.dialog.alert('Username: ' + this.username + '<br>Password: ' + this.password, () => {
+        this.$f7.loginScreen.close();
       });
     }
   }
+};
 </script>
